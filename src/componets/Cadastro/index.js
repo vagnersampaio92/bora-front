2654a2e640d381301839e4a8cb739c0b359136ca
vagnersampaio <vagnersampaio92@gmail.com>
2020-05-04 04:48:45 -0300

    cadastro de restaurantes

--- src/componets/Cadastro/index.js
+++ src/componets/Cadastro/index.js
@@ -12,12 +12,20 @@ export default class Historico extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            establishment_id: '',
-            name: '',
-            description: '',
-            photo_url: '',
-            price: '',
-            pratos: []
+            name:'',
+            email:'',
+            password:'',
+            description:'',
+            porcentagem:'',
+            min_order_price:'',
+            delivery_price:'',
+            min_delivery_minutes:'',
+            max_delivery_minutes:'',
+            banner:'',
+            phone:'',
+            address:'',
+            logo:'',
+            
 
 
         };
@@ -56,11 +64,20 @@ export default class Historico extends Component {
                             <div>
                                 <Form noValidate autoComplete="off">
                                     <div style={{ marginTop: 20, marginBottom: 30, borderColor: "#fa8e40" }}>
-                                        <h1>Cadastre</h1>
+                                        <h1>Cadastre o estabelecimento</h1>
                                         <TextField id="standard-basic" style={{ marginRight: 10, minWidth: 130 }} onChange={e => { this.setState({ name: e.target.value }) }} value={this.state.name} label="Nome" />
+                                        <TextField id="standard-basic" style={{ marginRight: 10, minWidth: 130 }} onChange={e => { this.setState({  email: e.target.value }) }} value={this.state.email} label="Email" />
+                                        <TextField id="standard-basic" style={{ marginRight: 10, minWidth: 130 }} onChange={e => { this.setState({ password: e.target.value }) }} value={this.state.password} label="Senha" />
                                         <TextField id="standard-basic" style={{ marginRight: 10, minWidth: 130 }} onChange={e => { this.setState({ description: e.target.value }) }} value={this.state.description} label="Descrição" />
-                                        <TextField id="standard-basic" style={{ marginRight: 10, minWidth: 130 }} onChange={e => { this.setState({ photo_url: e.target.value }) }} value={this.state.photo_url} label="Link da foto" />
-                                        <TextField id="standard-basic" style={{ marginRight: 10, minWidth: 130 }} onChange={e => { this.setState({ price: e.target.value }) }} value={this.state.price} label="Preço" />
+                                        <TextField id="standard-basic" style={{ marginRight: 10, minWidth: 130 }} onChange={e => { this.setState({ porcentagem: e.target.value }) }} value={this.state.porcentagem} label="Taxa" />
+                                        <TextField id="standard-basic" style={{ marginRight: 10, minWidth: 130 }} onChange={e => { this.setState({ min_order_price: e.target.value }) }} value={this.state.min_order_price} label="Menor valor do delivery" />
+                                        <TextField id="standard-basic" style={{ marginRight: 10, minWidth: 130 }} onChange={e => { this.setState({ delivery_price: e.target.value }) }} value={this.state.delivery_price} label="Valor do delivery" />
+                                        <TextField id="standard-basic" style={{ marginRight: 10, minWidth: 130 }} onChange={e => { this.setState({ min_delivery_minutes: e.target.value }) }} value={this.state.min_delivery_minutes} label="Menor tempo" />
+                                        <TextField id="standard-basic" style={{ marginRight: 10, minWidth: 130 }} onChange={e => { this.setState({ max_delivery_minutes: e.target.value }) }} value={this.state.max_delivery_minutes} label="Tempo máximo" />
+                                        <TextField id="standard-basic" style={{ marginRight: 10, minWidth: 130 }} onChange={e => { this.setState({ banner: e.target.value }) }} value={this.state.banner} label="Banner" />
+                                        <TextField id="standard-basic" style={{ marginRight: 10, minWidth: 130 }} onChange={e => { this.setState({ phone: e.target.value }) }} value={this.state.phone} label="Telefone" />
+                                        <TextField id="standard-basic" style={{ marginRight: 10, minWidth: 130 }} onChange={e => { this.setState({ address: e.target.value }) }} value={this.state.address} label="Endereço" />
+                                        <TextField id="standard-basic" style={{ marginRight: 10, minWidth: 130 }} onChange={e => { this.setState({ logo: e.target.value }) }} value={this.state.logo} label="Logo" />
                                         <Button variant="outlined" style={{ marginTop: 20, marginBottom: 20, borderColor: "#fa8e40" }} onClick={() => { this.salvar() }} style={{ marginTop: 15, }}>Cadastrar</Button>
                                     </div>
 
@@ -75,25 +92,7 @@ export default class Historico extends Component {
 
                         <Card>
 
-                            {this.state.pratos.map(prato => (
-                                <Card>
-
-                                    
-                                        <Img src = {prato.photo_url} ></Img>
-                                    <div >
-                                        <p>Nome:{prato.name}</p>
-                                        <p>Descrição:{prato.description}</p>
-                                        <p>Preço:R${prato.price/100}</p>
-                                        
-                                        
-       
-                                    </div>    
-
-
-
-                                </Card>
-                            ))
-                            }
+                          
                         </Card>
                     }
 
@@ -116,41 +115,37 @@ export default class Historico extends Component {
     async salvar() {
 
         const data = {}
-        data.establishment_id = sessionStorage.getItem('restauranteid')
-        data.name = this.state.name
-        data.description = this.state.description
-        data.photo_url = this.state.photo_url
-        data.price = this.state.price
+        data.name=this.state.name
+        data.email=this.state.email
+        data.password=this.state.password
+        data.city_id=1
+        data.description=this.state.description
+        data.porcentagem=this.state.porcentagem
+        data.min_order_price=this.state.min_order_price
+        data.delivery_price=this.state.delivery_price
+        data.avaliacao=1
+        data.min_delivery_minutes=this.state.min_delivery_minutes
+        data.max_delivery_minutes=this.state.max_delivery_minutes
+        data.status="close"
+        data.banner=this.state.banner
+        data.phone=this.state.phone
+        data.address=this.state.address
+        data.delivery_option="delivery"
+        data.logo=this.state.logo
+        data.rating="5"
 
 
+        console.log(data)
         try {
             //entrega
-            const response = await api.post('pratos', data)
+            const response = await api.post('signup', data)
             alert(" Cadastro feito")
 
         } catch (err) {
 
         }
     }
-    async listar() {
-        this.setState({ opcao: 2 })
-        const id = sessionStorage.getItem('restauranteid')
-
-        try {
-            //entrega
-            const response = await api.get('establishment/produtos/' + id)
-
-            this.setState({
-                pratos: response.data
-            })
-            console.log(this.state.pratos)
-
-        } catch (err) {
-
-        }
-
-
-    }
+  
 
 }
 
